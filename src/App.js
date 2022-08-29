3046b0f95a20773c2c95e9182e1eacf99d01eaf8
Minhaj Alam <97253984+minhaj840@users.noreply.github.com>
2022-08-29 18:02:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
+
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  const fetchData = e => {
+    const query = e.target.value
+    fetch(`https://jsonplaceholder.typicode.com/users?q=${query}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <h1> first write any word in Search box then it will list all data from api </h1>
+      <input onChange={fetchData} label="Search User" />
+      {users.length > 0 && (
+        <ul >
+          {users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
